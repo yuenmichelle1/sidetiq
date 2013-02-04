7f685969cb67e4253670aeb7fa2cdb6a0f7bf30e
Tobias Svensson <tob@tobiassvensson.co.uk>
2013-02-04 12:16:50 +0000

    Bump to 0.1.3.

--- lib/sidetiq/version.rb
+++ lib/sidetiq/version.rb
@@ -8,7 +8,7 @@ module Sidetiq
     MINOR = 1
 
     # Public: Sidetiq patch level.
-    PATCH = 2
+    PATCH = 3
 
     # Public: String representation of the current Sidetiq version.
     STRING = [MAJOR, MINOR, PATCH].compact.join('.')
