877a12a7ddb6b569fc2297921137cda992eeff12
Tobias Svensson <tob@tobiassvensson.co.uk>
2013-02-04 12:09:20 +0000

    Add documentation for Sidetiq::VERSION.

--- lib/sidetiq/version.rb
+++ lib/sidetiq/version.rb
@@ -1,9 +1,16 @@
 module Sidetiq
+  # Public: Sidetiq version namespace.
   module VERSION
+    # Public: Sidetiq major version number.
     MAJOR = 0
+
+    # Public: Sidetiq minor version number.
     MINOR = 1
+
+    # Public: Sidetiq patch level.
     PATCH = 2
 
+    # Public: String representation of the current Sidetiq version.
     STRING = [MAJOR, MINOR, PATCH].compact.join('.')
   end
 end
