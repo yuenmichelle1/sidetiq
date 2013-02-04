032c1d8d7fa016054d2dcd3ba06560c4ddf352d3
Tobias Svensson <tob@tobiassvensson.co.uk>
2013-02-04 12:15:57 +0000

    Add documentation for the Sidetiq config accessors.

--- lib/sidetiq/config.rb
+++ lib/sidetiq/config.rb
@@ -1,11 +1,23 @@
 module Sidetiq
   class << self
+    # Public: Sets the configuration used by Sidetiq.
     attr_writer :config
 
+    # Public: Configuration wrapper for block configurations.
+    #
+    # Examples
+    #
+    #   Sidetiq.configure do |config|
+    #     config.resolution = 0.2
+    #   end
+    #
+    # Yields the configuration OpenStruct currently set.
+    # Returns nothing.
     def configure
       yield config
     end
 
+    # Public: Returns the current configuration used by Sidetiq.
     def config
       @config ||= OpenStruct.new
     end
