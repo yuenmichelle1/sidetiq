14b12ed6dffe78fdde6badd60a26c17720964a5c
Tobias Svensson <tob@tobiassvensson.co.uk>
2013-03-11 11:17:29 +0000

    Don't use intervals in ScheduledWorker.

--- test/fixtures/scheduled_worker.rb
+++ test/fixtures/scheduled_worker.rb
@@ -3,11 +3,14 @@ class ScheduledWorker
   include Sidetiq::Schedulable
 
   tiq do
-    daily(1)
-    yearly(2)
-    monthly(3)
+    daily.hour_of_day(1)
+    yearly.month_of_year(2)
+    monthly.day_of_month(3)
 
     add_exception_rule yearly.month_of_year(:february)
+  end
+
+  def perform
   end
 end
 
